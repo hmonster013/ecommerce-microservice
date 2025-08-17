7fa8a4865e3f35d02e4f8456d1c9aeaad0c36e2a
hmonster013 <tranphuhuyit@gmail.com>
2025-08-17 22:44:48 +0700

    [FIX] Fix OpenAPI configuration

--- user-service/src/main/java/org/de013/userservice/UserServiceApplication.java
+++ user-service/src/main/java/org/de013/userservice/UserServiceApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.de013.common.config.SwaggerConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan(basePackages = {"org.de013.userservice", "org.de013.common"})
+@ComponentScan(
+    basePackages = {"org.de013.userservice", "org.de013.common"},
+    excludeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = SwaggerConfig.class
+    )
+)
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
